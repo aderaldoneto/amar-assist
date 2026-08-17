76a902f9c0c4de825256948a36d5dac7fdd7fcff
Aderaldo Neto <aderaldo.catarino@gmail.com>
2026-08-17 03:33:03 -0300

    feat: models

--- src/app/Models/ChargeDetail.php
+++ src/app/Models/ChargeDetail.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ChargeDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'charge_id',
+        'barcode',
+        'pix_key',
+        'card_holder_name',
+        'card_brand',
+        'card_last_four',
+    ];
+
+    public function charge(): BelongsTo
+    {
+        return $this->belongsTo(Charge::class);
+    }
 }
